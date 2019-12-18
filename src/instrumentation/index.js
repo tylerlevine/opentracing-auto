f84ed31963f4e992625c41217502cbc491cd30f8
Tyler Levine <tyler@bitgo.com>
2019-12-17 21:42:22 -0800

    Add instrumentation provider for mongodb 3.x

--- src/instrumentation/index.js
+++ src/instrumentation/index.js
@@ -3,6 +3,7 @@
 const express = require('./express')
 const expressError = require('./expressError')
 const httpClient = require('./httpClient')
+const mongodb = require('./mongodb')
 const mongodbCore = require('./mongodbCore')
 const mysql = require('./mysql')
 const pg = require('./pg')
@@ -13,6 +14,7 @@ module.exports = [
   express,
   expressError,
   httpClient,
+  mongodb,
   mongodbCore,
   mysql,
   pg,
